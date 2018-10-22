d06de6ff05bda3e6867c81cec65efdb4dfa4fbb2
imtiyazzaman1 <imtiyaz.zaman@live.co.uk>
2018-10-22 15:59:29 +0100

    Set up web project

---
+++ spec/spec_helper.rb
@@ -0,0 +1,18 @@
+require 'capybara/rspec'
+require 'simplecov'
+require 'simplecov-console'
+require 'capybara'
+require 'rspec'
+require './app'
+
+require File.join(File.dirname(__FILE__), '..', 'app.rb')
+
+ENV['RACK_ENV'] = 'test'
+Capybara.app = BookmarkManager
+
+SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new(
+  [
+    SimpleCov::Formatter::Console
+  ]
+)
+SimpleCov.start
