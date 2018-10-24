5fa6d7d2756844078c16c5ee43198dfe79e2797e
Edyta Bednarz <edy.b.bednarz@gmail.com>
2018-10-24 14:29:11 +0100

    Fixed redundant test and function

--- lib/bookmark.rb
+++ lib/bookmark.rb
@@ -1,16 +1,12 @@
 require 'pg'
 
 class Bookmark
-  attr_reader :name
+
 
   def self.all
     connection = PG.connect(dbname: 'bookmark_manager')
     result = connection.exec("SELECT * FROM bookmarks;")
     result.map { |bookmark| bookmark['url'] }
-  end
-
-  def initialize(name)
-    @name = name
   end
 
 end
--- spec/bookmark_spec.rb
+++ spec/bookmark_spec.rb
@@ -1,18 +1,10 @@
 require 'bookmark'
 
-describe Bookmark do
-  it 'initializes with a name' do
-    bookmark = Bookmark.new("Bookmark 1")
-    expect(bookmark.name).to eq "Bookmark 1"
+describe '.all' do
+  it 'returns a list of bookmarks' do
+    bookmarks = Bookmark.all
+
+    expect(bookmarks).to include "http://www.makersacademy.com"
+    expect(bookmarks).to include "http://www.google.com"
   end
-
-  describe '.all' do
-    it 'returns a list of bookmarks' do
-      bookmarks = Bookmark.all
-
-      expect(bookmarks).to include "http://www.makersacademy.com"
-      expect(bookmarks).to include "http://www.google.com"
-    end
-  end
-
 end
